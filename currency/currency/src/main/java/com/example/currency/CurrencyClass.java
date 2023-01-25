e6959715cf982f5c6f50a7c981c6a16d196c3237
PrincessCrumb <121231819+PrincessCrumb@users.noreply.github.com>
2023-01-25 16:53:58 +0400

    Add files via upload

---
+++ currency/currency/src/main/java/com/example/currency/CurrencyClass.java
@@ -0,0 +1,14 @@
+package com.example.currency;
+
+public class CurrencyClass {
+    public final String symbol;
+    public final String name;
+    public final long value;
+
+    public CurrencyClass(String symbol, String name, long value){
+        this.symbol=symbol;
+        this.name=name;
+        this.value=value;
+    }
+
+}
